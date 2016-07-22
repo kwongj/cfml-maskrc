622cd5ea39d86a931fbbccc8338836d99886d8c5
Jason Kwong <kwongj@gmail.com>
2016-07-23 01:08:24 +1000

    Added colour option; now distinguishes extant from ancestral recombination

--- cfml-maskrc.py
+++ cfml-maskrc.py
@@ -2,6 +2,8 @@
 # Script by Jason Kwong
 # Script to mask recombination from CFML output and draw SVG of recombinant regions
 
+from __future__ import print_function
+
 # Usage
 import argparse
 from argparse import RawTextHelpFormatter
@@ -18,11 +20,12 @@ parser.add_argument('--regions', metavar='FILE', help='output recombinant region
 parser.add_argument('--svg', metavar='FILE', help='draw SVG output of recombinant regions and save as specified file')
 parser.add_argument('--svgsize', metavar='WIDExHIGH', default='800x600', help='specify width and height of SVG in pixels (default="800x600")')
 parser.add_argument('--svgorder', metavar='FILE', help='specify file containing list of taxa (1 per line) in desired order')
+parser.add_argument('--svgcolour', metavar='COLOUR', default='black', help='specify colour of recombination regions in HEX format (default=black)')
 parser.add_argument('--consensus', action='store_true', help='add consensus row of recombination hotspots')
 parser.add_argument('--version', action='version', version=
 	'=====================================\n'
-	'%(prog)s v0.1\n'
-	'Updated 6-Oct-2015 by Jason Kwong\n'
+	'%(prog)s v0.2\n'
+	'Updated 22-Jul-2016 by Jason Kwong\n'
 	'Dependencies: Python 2.x, BioPython, ete2, svgwrite\n'
 	'=====================================')
 args = parser.parse_args()
@@ -44,13 +47,16 @@ from Bio.SeqRecord import SeqRecord
 import svgwrite
 
 # Functions
-def progexit(n):
-	sys.exit(n)
+def msg(*args, **kwargs):
+	print(*args, file=sys.stderr, **kwargs)
+
+def err(*args, **kwargs):
+	msg(*args, **kwargs)
+	sys.exit(1);
 
 def check_file(f):
 	if os.path.isfile(f) == False:
-		print 'ERROR: Cannot find "%s". Check CFML output files exist in this directory.' % f
-		progexit(1)
+		err('ERROR: Cannot find "{}". Check CFML output files exist in this directory.'.format(f))
 
 # Check input files (output from CFML)
 check_file(cfmlTREE)
@@ -82,11 +88,13 @@ with open(cfmlRECOMB) as csvfile:
 		node = t.search_nodes(name=seq)[0]
 		if node.is_leaf():
 			leaf_names = [node.name]
+			z = 'extant'
 		else:
 			leaves = node.get_leaves()
 			leaf_names = [leaf.name for leaf in leaves]
+			z = 'ancestral'
 		for l in leaf_names:
-			seqLIST.append([l, [RCstart, RCstop]])
+			seqLIST.append([l, [RCstart, RCstop, z]])
 
 # Build dictionary of recombinant regions
 for k,v in seqLIST:
@@ -95,20 +103,20 @@ for k,v in seqLIST:
 # Mask recombination in sequences
 seqALN = []
 for record in SeqIO.parse(args.aln, 'fasta'):
-	print 'Reading %s ... ' % record.id
+	msg('Reading {} ... '.format(record.id))
 	seqlen = len(record.seq)
 	regions = d.get(record.id, None)
 	newrec = MutableSeq(str(record.seq))
 	if regions:
 		for a in regions:
 			start = int(a[0]) - 1
-			end = int(a[1])		
+			end = int(a[1])
 			lenMASK = end - start
 			newrec[start:end] = (symbol)*lenMASK
 	seqALN.append(SeqRecord(Seq(str(newrec)), record.id, description=''))
 
 # Write masked alignment to file
-print 'Writing masked alignment to %s ... ' % outfile
+msg('Writing masked alignment to {} ... '.format(outfile))
 SeqIO.write(seqALN, outfile, 'fasta')
 
 # Write recombinant regions to file
@@ -119,48 +127,52 @@ if args.regions:
 			for b in v:
 				x = b[0]
 				y = b[1]
-				csvwriter.writerow([k,x,y])
+				z = b[2]
+				csvwriter.writerow([k,x,y,z])
 
 # Draw SVG
 svgsize = args.svgsize.split('x',1)
 width = int(svgsize[0])
 height = int(svgsize[1])
-font = 'font-size:10px; font-family:Arial'
-colour = 'black'
 numseqs = len(leafLIST)
 h = height/float(numseqs)
 s = width/float(int(seqlen))
+fsize = int(h*0.9)
+font = 'font-size:{}px; font-family:Arial'.format(fsize)
+colour = args.svgcolour
+main_colour = 'black'
+ancestral_colour = '#A9A9A9'
 interval = 500000		# Tick interval in bp
 
 dwg = svgwrite.Drawing(args.svg)
 
-def rect(x,p,w):		# Draw regions
+def rect(x,p,w,colour):		# Draw regions
 	dwg.add(dwg.rect(insert=((x*s)+5, (p*h)+5), size=(w, h*0.8), fill=colour))
 	if args.consensus:
-		dwg.add(dwg.rect(insert=((x*s)+5, 5), size=(w, h*0.8), fill=colour))
+		dwg.add(dwg.rect(insert=((x*s)+5, 5), size=(w, h*0.8), fill=main_colour))
 
 def label(n,p):			# Sequence ID labels
-	dwg.add(dwg.text(n, insert=((seqlen*s)+15, (((p+1)*h)-(0.2*h))+5), fill=colour, style=font))
+	dwg.add(dwg.text(n, insert=((seqlen*s)+15, (((p+1)*h)-(0.2*h))+5), fill=main_colour, style=font))
 
 def ticks(q):			# Draw tick marks
 	count = interval
 	while count < seqlen:
-		dwg.add(dwg.line((count*s,((q)*h)), (count*s,((q)*h)-3), stroke=colour))
-		dwg.add(dwg.text((count/1000000.0), insert=((count*s)-6, ((q)*h)+13), fill=colour, style=font))
+		dwg.add(dwg.line((count*s,((q)*h)), (count*s,((q)*h)-3), stroke=main_colour))
+		dwg.add(dwg.text((count/1000000.0), insert=((count*s)-6, ((q)*h)+13), fill=main_colour, style=font))
 		count = count + interval
 
 def box(width,h):		# Box with 5px margin
-	dwg.add(dwg.line((0,0), ((width+10),0), stroke=colour))
-	dwg.add(dwg.line(((width+10),0), ((width+10),((p+1)*h)), stroke=colour))
-	dwg.add(dwg.line(((width+10),((p+1)*h)), (0,((p+1)*h)), stroke=colour))
-	dwg.add(dwg.line((0,((p+1)*h)), (0,0), stroke=colour))	
+	dwg.add(dwg.line((0,0), ((width+10),0), stroke=main_colour))
+	dwg.add(dwg.line(((width+10),0), ((width+10),((p+1)*h)), stroke=main_colour))
+	dwg.add(dwg.line(((width+10),((p+1)*h)), (0,((p+1)*h)), stroke=main_colour))
+	dwg.add(dwg.line((0,((p+1)*h)), (0,0), stroke=main_colour))
 
 # Draw recombinant regions and labels
 if args.svg:
-	print 'Drawing SVG to %s ...' % args.svg
+	msg('Drawing SVG to {} ...'.format(args.svg))
 	if args.consensus:
 		p = 1
-		dwg.add(dwg.text('Consensus', insert=((seqlen*s)+15, (0.8*h)+5), fill=colour, style=font))
+		dwg.add(dwg.text('Consensus', insert=((seqlen*s)+15, (0.8*h)+5), fill=main_colour, style=font))
 	else:
 		p = 0
 	for seq in leafLIST:
@@ -169,10 +181,14 @@ if args.svg:
 			for b in v:
 				x = int(b[0])
 				y = int(b[1])
+				z = b[2]
 				w = (y-x)*s
 				if w < 1:
 					w = 1
-				rect(x,p,w)		# regions
+				if z == 'extant':
+					rect(x,p,w,colour)		# regions
+				elif z == 'ancestral':
+					rect(x,p,w,ancestral_colour)
 		label(seq,p)			# labels
 		p = p+1
 	# Draw bounding box
@@ -182,5 +198,5 @@ if args.svg:
 	dwg.save()
 
 # Exit
-print 'Done.'
-progexit(0)
+msg('Done.')
+sys.exit(0)
